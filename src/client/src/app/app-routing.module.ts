eda91bb882630f81cd9279077ae67fe5a1f60d14
Geert Huls <geert.huls82@gmail.com>
2019-10-14 21:13:56 +0200

    Add compontents and routes

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { MoviesComponent } from './movies/movies.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'movies', component: MoviesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -7,10 +7,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
